1aac1df97b3fdb8e1e67719e27b2f171d3313e90
anvar2404 <anvarabdullaev14@gmail.com>
2020-09-08 12:01:42 +0600

    files commit

---
+++ client/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <title>To Do List</title>
+      <header className="bold text-xl text-center mb-12">My To Do List</header>
+    </div>
+  )
+}
+
+export default Header
